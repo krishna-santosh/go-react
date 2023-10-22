460773d473dd17b89759d8bff9f901c6209712d3
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2023-10-22 17:40:17 +0530

    feat: add vite-pwa config

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Go React',
+        short_name: 'Go React',
+        description: 'Go React',
+        id: "/",
+        theme_color: '#ffffff',
+        display: "standalone",
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: '/vite.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 })
