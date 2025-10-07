aa0a2af43615384828d6e6f547509fdd84ca573a
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-10-07 21:29:49 +0530

    feat: add server proxy configuration for API requests

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -16,4 +16,12 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+        changeOrigin: true,
+      },
+    },
+  },
 });
