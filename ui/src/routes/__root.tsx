1c527675fb9943eb34b73f3f06939831a1867d43
Krishna Santosh <75202541+krishna-santosh@users.noreply.github.com>
2025-10-07 09:31:11 +0530

    fix: remove Header

--- ui/src/routes/__root.tsx
+++ ui/src/routes/__root.tsx
@@ -2,8 +2,6 @@ import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 
-import Header from "../components/Header";
-
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 
 import type { QueryClient } from "@tanstack/react-query";
@@ -15,7 +13,6 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
-      <Header />
       <Outlet />
       <TanStackDevtools
         config={{
